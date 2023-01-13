bb86f8efa6a5ffeb4d35057f504c8f8bcbf48c3b
Zouv4ik <prostozzouv4ik@gmail.com>
2023-01-13 19:41:41 +0300

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <Tutorial/>
+  <h1>Hello!</h1>
 </template>
 
 <script>
+import { PrismaClient } from '@prisma/client' 
+
+const prisma = new PrismaClient({
+  
+})
+
 export default {
   name: 'IndexPage'
+
 }
 </script>
